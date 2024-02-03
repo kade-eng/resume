828f98305fb36a0c47263a37848e1424de66a739
Kade BC <kade.w.bc@gmail.com>
2024-02-03 18:12:47 -0500

    add cors to cf

--- serverless/index.js
+++ serverless/index.js
@@ -1,9 +1,13 @@
+const cors = require('cors')({origin: 'https://kade-bc.com'});
 const {Firestore} = require('@google-cloud/firestore');
 const firestore = new Firestore();
 
-exports.visitCounter = async (req, res) => {
-  const docRef = firestore.collection('visitors').doc('counter');
-  await docRef.update({ count: Firestore.FieldValue.increment(1) });
-  const doc = await docRef.get();
-  res.status(200).send({ count: doc.data().count });
+exports.visitCounter = (req, res) => {
+  cors(req, res, async () => {
+    const docRef = firestore.collection('visitors').doc('counter');
+    await docRef.update({ count: Firestore.FieldValue.increment(1) });
+
+    const doc = await docRef.get();
+    res.status(200).send({ count: doc.data().count });
+  });
 };
