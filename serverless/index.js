f077b1943bf4bbbd186e8bdbcab307f6a88591d5
Kade BC <kade.w.bc@gmail.com>
2024-02-03 23:24:42 -0500

    update cf

--- serverless/index.js
+++ serverless/index.js
@@ -4,6 +4,13 @@ const firestore = new Firestore();
 
 exports.visitCounter = (req, res) => {
   cors(req, res, async () => {
+    if (req.method === 'OPTIONS') {
+      res.status(204).send('');
+      return;
+    }
+
+    res.set('Access-Control-Allow-Origin', '*');
+
     const docRef = firestore.collection('visitors').doc('counter');
     await docRef.update({ count: Firestore.FieldValue.increment(1) });
 
